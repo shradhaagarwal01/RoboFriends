ebcab43d30c940dc2cb5590f75c28bf741743124
shradhaagarwal01 <62115289+shradhaagarwal01@users.noreply.github.com>
2020-09-22 07:20:42 +0530

    commit my files

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React,{ Component } from 'react';
+import { robots } from './robots';
+import CardList from './CardList';
+// import SearchBox from './SearchBox';
+import './App.css';
+
+class App extends Component{
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+    submitHandler = (event) => {
+        alert(`${this.state.searchfield}`)
+        event.preventDefault()
+    }
+
+    
+
+    render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+        return (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <div>
+                    <input type="text" value={this.state.searchfield} onChange = {this.onSearchChange}/>
+                    <button type="submit" onClick={this.submitHandler}>Search</button>
+                </div>
+                <CardList robots = {filteredRobots}/>
+                
+            </div>
+        );
+
+    }
+}
+
+export default App 
